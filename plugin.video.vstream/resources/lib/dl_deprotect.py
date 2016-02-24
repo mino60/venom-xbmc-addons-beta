b08b5ab9f66facc37e3963d5fadec7e0af4862e9
TmpName <gihubtmp@yopmail.com>
2016-02-24 22:55:28 +0100

    Update dl_deprotect.py

--- plugin.video.vstream/resources/lib/dl_deprotect.py
+++ plugin.video.vstream/resources/lib/dl_deprotect.py
@@ -5,38 +5,37 @@ from base64 import urlsafe_b64encode
 import urllib2,xbmc,urllib,re
 from urllib2 import URLError
 
-
-
-#Cette fonction n'est pas encore utilisée, servira le jour ou dl-protect re-activera le captcha
-# def get_response(img):
-    # try:
-        # img = xbmcgui.ControlImage(450, 0, 400, 130, img)
-        # wdlg = xbmcgui.WindowDialog()
-        # wdlg.addControl(img)
-        # wdlg.show()
-        # #xbmc.sleep(3000)
-        # kb = xbmc.Keyboard('', 'Type the letters in the image', False)
-        # kb.doModal()
-        # if (kb.isConfirmed()):
-            # solution = kb.getText()
-            # if solution == '':
-                # raise Exception('You must enter text in the image to access video')
-            # else:
-                # return solution
-        # else:
-            # raise Exception('Captcha Error')
-    # finally:
-        # wdlg.close()
+def get_response(img):
+    import xbmcgui,xbmc
+    try:
+        img = xbmcgui.ControlImage(450, 0, 400, 130, img)
+        wdlg = xbmcgui.WindowDialog()
+        wdlg.addControl(img)
+        wdlg.show()
+        #xbmc.sleep(3000)
+        kb = xbmc.Keyboard('', 'Type the letters in the image', False)
+        kb.doModal()
+        if (kb.isConfirmed()):
+            solution = kb.getText()
+            if solution == '':
+                raise Exception('You must enter text in the image to access video')
+            else:
+                return solution
+        else:
+            raise Exception('Captcha Error')
+    finally:
+        wdlg.close()
 
 
 def DecryptDlProtect(url):
+
     if not (url): return ''
     
     headers = {
     'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:37.0) Gecko/20100101 Firefox/37.0',
     'Referer' : url ,
     'Host' : 'www.dl-protect.com',
-    #'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
     'Accept-Language': 'en-gb, en;q=0.9',
     'Pragma' : '',
     'Accept-Charset' : '',
@@ -48,6 +47,13 @@ def DecryptDlProtect(url):
     except URLError, e:
         print e.read()
         print e.reason
+        return ''
+    
+    #Si redirection
+    UrlRedirect = reponse.geturl()
+    if not(UrlRedirect == url):
+        reponse.close()
+        return UrlRedirect
         
     sHtmlContent = reponse.read()
     
@@ -57,14 +63,60 @@ def DecryptDlProtect(url):
     cookies = '__cfduid=' + str(c2[0][0]) + ';cu=' + str(c2[0][1]) + ';PHPSESSID=' + str(c2[0][2])
     
     reponse.close()
-        
-    key = re.findall('input name="key" value="(.+?)"',sHtmlContent)
     
-    #Ce parametre ne sert pas encore pour le moment
-    mstime = int(round(time() * 1000))
-    b64time = "_" + urlsafe_b64encode(str(mstime)).replace("=", "%3D")
+    #test si captcha demande
+    if '<td align=center> Please enter the characters from the picture to see the links </td>' in sHtmlContent:
+        print 'captcha'
+        print url
+        s = re.findall('<img id="captcha" alt="Security code" src="([^<>"]+?)"',sHtmlContent)
+        image = 'http://www.dl-protect.com' + s[0]
+        
+        captcha = get_response(image)
+        
+        key = re.findall('name="key" value="(.+?)"',sHtmlContent)
+        
+        #Ce parametre ne sert pas encore
+        mstime = int(round(time() * 1000))
+        b64time = "_" + urlsafe_b64encode(str(mstime)).replace("=", "%3D")
+        
+        query_args = ( ('key' , key[0] ) , ( 'i' , b64time) , ('secure' , captcha ), ( 'submitform' , 'Valider')  )
+        print query_args
+        
+        data = urllib.urlencode(query_args)
+    
+        #rajout des cookies
+        headers.update({'Cookie': cookies})
+
+        request = urllib2.Request(url,data,headers)
+
+        try: 
+            reponse = urllib2.urlopen(request)
+        except URLError, e:
+            print e.read()
+            print e.reason
+            
+        sHtmlContent = reponse.read()
+        reponse.close()
+        
+        #fh = open('c:\\test.txt', "w")
+        #fh.write(sHtmlContent)
+        #fh.close()
+        
+        return sHtmlContent
+    
 
     if 'Please click on continue to see' in sHtmlContent:
+        
+        key = re.findall('input name="key" value="(.+?)"',sHtmlContent)
+    
+        #Ce parametre ne sert pas encore pour le moment
+        mstime = int(round(time() * 1000))
+        b64time = "_" + urlsafe_b64encode(str(mstime)).replace("=", "%3D")
+        
+        #fh = open('c:\\test.txt', "w")
+        #fh.write(sHtmlContent)
+        #fh.close()
+              
         #tempo necessaire
         cGui().showInfo("Patientez", 'Decodage en cours' , 2)
         xbmc.sleep(1000)
@@ -87,6 +139,8 @@ def DecryptDlProtect(url):
         
         reponse.close()
     
-    return sHtmlContent
+        return sHtmlContent
+        
+    return ''
         
  
